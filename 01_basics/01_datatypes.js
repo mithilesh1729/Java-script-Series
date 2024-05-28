b2ee19e28a3621b7054903c5025f3154a57405ca
Mithilesh Kumar <131949594+mithilesh1729@users.noreply.github.com>
2024-05-28 14:23:09 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs ,not browser
+
+// console.log(3+3)
+
+let name="Mithilesh"
+let age=20
+let isLoggedIn=true
+
+//number
+//bigint
+//string=" "
+//boolean ----> true or false
+// null  --->standalone value
+//undefined ---> value not assigned
+//symbol  ---> uniqueness
+
+console.log(typeof null); //----->object type
+console.log(typeof undefined); // undefined type 
